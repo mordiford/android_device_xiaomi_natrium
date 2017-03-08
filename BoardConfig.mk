e691acbb60f526a4548d3fea6e87d8ce3f728f91
LuK1337 <priv.luk@gmail.com>
2017-03-08 01:10:22 +0100

    capricorn: Use gemini defconfig for now
    Change-Id: Ia8820df911dd54f6e46b577726d4fb1d4e7fdd76

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,5 +27,8 @@ TARGET_OTA_ASSERT_DEVICE := capricorn
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
+# Kernel
+TARGET_KERNEL_CONFIG := lineageos_gemini_defconfig
+
 # inherit from the proprietary version
 -include vendor/xiaomi/capricorn/BoardConfigVendor.mk
