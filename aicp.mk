ab94b7644939275ef461c96cc2a0b08b160b9c70
lindwurm <lindwurm.q@gmail.com>
2017-03-13 20:46:06 +0900

    capricorn: AICP-ify
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- aicp.mk
+++ aicp.mk
@@ -1,15 +1,27 @@
 $(call inherit-product, device/xiaomi/capricorn/full_capricorn.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
-PRODUCT_NAME := lineage_capricorn
+# Inherit telephony stuff
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
+
+PRODUCT_NAME := aicp_capricorn
 PRODUCT_DEVICE := capricorn
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Mi 5s
 PRODUCT_MANUFACTURER := xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        DEVICE_MAINTAINERS="lindwurm"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/configs/bootanimation.mk
 
 # PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="natrium" \
---
+++ system_prop.mk
@@ -0,0 +1,8 @@
+# Hardware Info
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.device.chipset=Qualcomm MSM8996 Snapdragon 821 \
+    ro.device.cpu=Quad-core (2x2.15 GHz Kryo & 2x1.6 GHz Kryo) \
+    ro.device.gpu=Adreno 530 \
+    ro.device.rear_cam=12 MP \
+    ro.device.front_cam=4 MP \
+    ro.device.screen_res=1080 x 1920
